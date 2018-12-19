14c12788b09edde0e004435cf533aca536b98563
BETTINGER SEBASTIEN <sebastien.bettinger@open-groupe.com>
2018-12-19 15:48:50 +0100

    Add userTokens

---
+++ api/userTokens.js
@@ -0,0 +1,18 @@
+var Client = require('node-rest-client').Client;
+
+let usertokens = function(api) {
+    exports.search = function(login) {
+        return api.get('/api/user_tokens/search' + ((login!== undefined)?'?login=' + login:''));
+    };
+
+    exports.generate = function(name, login) {
+        return api.post('/api/user_tokens/generate', {
+            name: name,
+            login: login
+        });
+    }
+    return this;
+};
+exports.usertokens = usertokens;
+
+
--- sonarQube.js
+++ sonarQube.js
@@ -1,20 +1,24 @@
 var Client = require('node-rest-client').Client;
-var Common = require('./api/commons');
+var Common = require('./api/commons').common();
 var permissions =require('./api/permissions');
 var userGroup = require('./api/userGroups');
+var userTokens = require('./api/userTokens');
 
 let sonarQube = function(auth, server){
     this.auth = auth;
     this.server = server;
     let client = new Client(auth);
 
-    let api_ = Common.common().api(client, server);
+    let api_ = Common.api(client, server);
 
     let permission = permissions.permission(api_);
     exports.permission = permission;
 
     let user_group = userGroup.usergroups(api_);
     exports.user_group = user_group;
+
+    let user_tokens = userTokens.usertokens(api_);
+    exports.user_tokens = user_tokens;
 
     return this;
 };
